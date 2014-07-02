1f4f3087c4ea3b3bf38eeb77e9ab80c9e2ff242f
T Beseda <tbeseda@gmail.com>
2014-07-02 16:27:47 -0600

    catch return in search and add extra return to comment

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
   return {
     events: {
       'app.activated': 'init',
+      'keyup #search-field': 'search_keyup',
       'click #gifme': 'gifme',
       'get_gifs.done': 'gifs_got',
       'click img.gif': 'use_gif'
@@ -30,6 +31,12 @@
       this.switchTo('search', {});
     },
 
+    search_keyup: function(e){
+      if (e.which == 13) {
+        this.$('#gifme').click()
+      }
+    },
+
     gifme: function(){
       this.search_term = this.$('#search-field').val();
       this.ajax('get_gifs');
@@ -41,7 +48,9 @@
 
     use_gif: function(e){
       var comment = this.comment();
-      comment.text(comment.text() + '\n ![]('+this.$(e.currentTarget).attr('src')+')');
+      var text = comment.text();
+      text = text.length > 0 ? text+'\n\n' : '';
+      comment.text(text+'![]('+this.$(e.currentTarget).attr('src')+')');
       this.switchTo('search', {});
     }
 
