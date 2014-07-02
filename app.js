77e23bebd2c2ac6fab69beee6833d3b9dcf9c2d7
T Beseda <tbeseda@gmail.com>
2014-07-02 11:56:20 -0600

    testing AJAX call with Zendesk's janky request system

--- app.js
+++ app.js
@@ -2,30 +2,52 @@
 
   'use strict';
 
+  var API_URL = 'http://api.giphy.com/v1/gifs';
+  var API_KEY = 'dc6zaTOxFJmzC';
+
   return {
     events: {
       'app.activated': 'init', // This event fires when App is activated.
-      'pane.activated': 'paneOnActivated', // This event fires when pane in topbar is activated.
-      'pane.deactivated': 'paneOnDeactivated',
-      'click #gifme': 'gifme'
+      'pane.activated': 'activate_pane', // This event fires when pane in topbar is activated.
+      'pane.deactivated': 'deactivate_pane',
+      'click #gifme': 'gifme',
+      'get_gifs.done': 'gifs_got'
     },
+
+    requests: {
+      get_gifs: function(){
+        console.log('Search term is set to:', this.search_term);
+        return {
+          url: API_URL+'/search/',
+          type: 'GET',
+          data: {
+            api_key: API_KEY,
+            q = this.search_term
+          }
+        }
+      }
+    }
 
     init: function(){
 
     },
 
-    paneOnActivated: function(){
+    activate_pane: function(){
       this.switchTo('search', {});
       this.popover({width: 600, height: 400});
     },
 
-    paneOnDeactivated: function(){
+    deactivate_pane: function(){
 
     },
 
     gifme: function(){
-      var search_term = this.$('#search-field').val();
-      console.log(search_term);
+      this.search_term = this.$('#search-field').val();
+      this.ajax('get_gifs');
+    },
+
+    gifs_got: function(response){
+      console.log('Got gifs?', response);
     }
 
   };
